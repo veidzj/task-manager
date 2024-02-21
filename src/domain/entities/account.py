8ff32cd31142cac0a641d82acfb4658a89a03a79
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 20:28:13 -0300

    refactor: add custom ValidationError to Account

--- src/domain/entities/account.py
+++ src/domain/entities/account.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import uuid
 import re
+from src.domain.errors.validation_error import ValidationError
 
 class Account:
     def __init__(self, email, password):
@@ -14,8 +15,8 @@ class Account:
 
     def validate_email(self, email):
         if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            raise ValueError('Email must be a valid email')
+            raise ValidationError('Email must be a valid email')
 
     def validate_password(self, password):
         if len(password) < 6 or len(password) > 255:
-            raise ValueError('Password must be between 6 and 255 characters long')
+            raise ValidationError('Password must be between 6 and 255 characters long')
--- tests/domain/entities/test_account.py
+++ tests/domain/entities/test_account.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from uuid import UUID
 from faker import Faker
 from src.domain.entities.account import Account
+from src.domain.errors.validation_error import ValidationError
 
 faker = Faker()
 
@@ -17,11 +18,11 @@ def test_account_creation():
     assert isinstance(account.createdAt, datetime)
 
 def test_invalid_email():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValidationError):
         Account(faker.name(), faker.password())
 
 def test_invalid_password():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValidationError):
         Account(faker.email(), faker.pystr(1, 5))
-    with pytest.raises(ValueError):
+    with pytest.raises(ValidationError):
         Account(faker.email(), faker.pystr(256, 257))
