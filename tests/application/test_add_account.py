82e6ae20a18391bf8eee4c51708e9ba0ff8418a7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 20:59:21 -0300

    refactor: change data to use faker

--- tests/application/test_add_account.py
+++ tests/application/test_add_account.py
@@ -1,17 +1,19 @@
 import pytest
+from faker import Faker
 from unittest.mock import Mock
 from werkzeug.security import generate_password_hash
 from src.application.add_account import AddAccount
 from src.domain.errors.account_already_exists_error import AccountAlreadyExistsError
+
+faker = Faker()
+email = faker.email()
+password = faker.password()
 
 def test_add_new_account_success():
     mock_get_account_by_email_repository = Mock()
     mock_add_account_repository = Mock()
     mock_get_account_by_email_repository.get_by_email.return_value = None
     add_account = AddAccount(mock_get_account_by_email_repository, mock_add_account_repository)
-
-    email = "test@example.com"
-    password = "password123"
 
     add_account.add(email, password)
 
@@ -23,12 +25,9 @@ def test_add_account_fails_when_account_exists():
     mock_get_account_by_email_repository = Mock()
     mock_add_account_repository = Mock()
 
-    mock_get_account_by_email_repository.get_by_email.return_value = {"email": "test@example.com", "password": "hashed_password"}
+    mock_get_account_by_email_repository.get_by_email.return_value = {'email': email, 'password': password}
 
     add_account = AddAccount(mock_get_account_by_email_repository, mock_add_account_repository)
-
-    email = "test@example.com"
-    password = "password123"
 
     with pytest.raises(AccountAlreadyExistsError):
         add_account.add(email, password)
