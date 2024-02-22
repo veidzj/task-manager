f2f6f9f7b545a9a40e0b72e632a865cf4ed6f89e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 22:15:53 -0300

    test: ensure sign up route returns 400 if AddAccount raises a ValidationError

--- tests/controllers/test_auth_controller.py
+++ tests/controllers/test_auth_controller.py
@@ -2,6 +2,7 @@ import pytest
 from faker import Faker
 from src.api.app import app
 from unittest.mock import patch
+from src.domain.errors.validation_error import ValidationError
 
 faker = Faker()
 email = faker.email()
@@ -21,3 +22,10 @@ def test_sign_up_success(client):
         response = client.post('/v1/sign-up', json={'email': email, 'password': password})
         assert response.status_code == 201
         assert 'accessToken' in response.json['data']
+
+def test_sign_up_validation_error(client):
+    with patch('src.application.add_account.AddAccount.add') as mock_add_account, \
+         patch('src.application.authentication.Authentication.handle') as mock_authentication:
+        mock_add_account.side_effect = ValidationError(faker.words())
+        response = client.post('/v1/sign-up', json={'email': email, 'password': password})
+        assert response.status_code == 400
