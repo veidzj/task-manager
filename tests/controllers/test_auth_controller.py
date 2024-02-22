000373bba335513ec54352a85be27114eab85da5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 22:21:27 -0300

    refactor: remove unused mock

--- tests/controllers/test_auth_controller.py
+++ tests/controllers/test_auth_controller.py
@@ -25,22 +25,19 @@ def test_sign_up_success(client):
         assert 'accessToken' in response.json['data']
 
 def test_sign_up_validation_error(client):
-    with patch('src.application.add_account.AddAccount.add') as mock_add_account, \
-         patch('src.application.authentication.Authentication.handle') as mock_authentication:
+    with patch('src.application.add_account.AddAccount.add') as mock_add_account:
         mock_add_account.side_effect = ValidationError(faker.words())
         response = client.post('/v1/sign-up', json={'email': email, 'password': password})
         assert response.status_code == 400
 
 def test_sign_up_account_already_exists_error(client):
-    with patch('src.application.add_account.AddAccount.add') as mock_add_account, \
-         patch('src.application.authentication.Authentication.handle') as mock_authentication:
+    with patch('src.application.add_account.AddAccount.add') as mock_add_account:
         mock_add_account.side_effect = AccountAlreadyExistsError()
         response = client.post('/v1/sign-up', json={'email': email, 'password': password})
         assert response.status_code == 401
 
 def test_sign_up_unexpected_error(client):
-    with patch('src.application.add_account.AddAccount.add') as mock_add_account, \
-         patch('src.application.authentication.Authentication.handle') as mock_authentication:
+    with patch('src.application.add_account.AddAccount.add') as mock_add_account:
         mock_add_account.side_effect = TypeError()
         response = client.post('/v1/sign-up', json={'email': email, 'password': password})
         assert response.status_code == 500
