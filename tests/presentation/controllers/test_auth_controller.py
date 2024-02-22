d0bc664855509f5644ef7a59accca9beda2c4d60
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-21 23:00:42 -0300

    test: ensure sign up route returns status 404 if Authentication raises AccountNotFoundError

--- tests/presentation/controllers/test_auth_controller.py
+++ tests/presentation/controllers/test_auth_controller.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 from src.domain.errors.validation_error import ValidationError
 from src.domain.errors.account_already_exists_error import AccountAlreadyExistsError
 from src.domain.errors.invalid_credentials_error import InvalidCredentialsError
+from src.domain.errors.account_not_found_error import AccountNotFoundError
 
 faker = Faker()
 email = faker.email()
@@ -42,6 +43,14 @@ def test_sign_up_invalid_credentials_error(client):
         mock_authentication.side_effect = InvalidCredentialsError()
         response = client.post('/v1/sign-up', json={'email': email, 'password': password})
         assert response.status_code == 401
+
+def test_sign_up_account_not_found_error(client):
+    with patch('src.application.add_account.AddAccount.add') as mock_add_account, \
+         patch('src.application.authentication.Authentication.auth') as mock_authentication:
+        mock_add_account.return_value = None
+        mock_authentication.side_effect = AccountNotFoundError()
+        response = client.post('/v1/sign-up', json={'email': email, 'password': password})
+        assert response.status_code == 404
 
 def test_sign_up_unexpected_error(client):
     with patch('src.application.add_account.AddAccount.add') as mock_add_account:
